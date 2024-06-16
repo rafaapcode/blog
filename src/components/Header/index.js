1673ffd5ea9236673e9afcf6238e126d57e1e9d3
rafaapcode <rafaap2003@gmail.com>
2024-06-16 17:05:23 -0300

    feature: styling the header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,5 +1,24 @@
+import LogoSvg from "../../svg/logo.svg";
+
 export default function Header() {
+
+  const descLogo = "Esssa é a logo do blog";
+
   return (
-    <h1>Header</h1>
+    <>
+      <header className="flex-space-between">
+        <div className="logo">
+          <img src={LogoSvg} alt={descLogo}/>
+        </div>
+        <div className="search">
+          <input type="text" className="input-search" name="search" />
+        </div>
+        <ul className="menu">
+          <li><a href="#teste" className="nav-link">Categories</a></li>
+          <li><a href="#teste" className="nav-link">About</a></li>
+          <li><a href="#teste" className="nav-link">Contact</a></li>
+        </ul>
+      </header>
+    </>
   )
 }
