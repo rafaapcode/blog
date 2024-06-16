a4dae15640e7f5ecf6cc5f676e8e0e06cb3a211c
rafaapcode <rafaap2003@gmail.com>
2024-06-16 16:41:44 -0300

    chore: creating the styles variables and patterns

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
+import Header from "./components/Header";
+import Article from "./components/Article";
+
 function App() {
   return (
-   <h1>My blog</h1>
+    <>
+      <Header />
+      <Article />
+    </>
   );
 }
 
---
+++ src/components/Article/index.js
@@ -0,0 +1,11 @@
+import Posts from "../Posts"
+
+export default function Article(){
+  return (
+    <>
+      <Posts />
+      <h1>Article</h1>
+    </>
+  )
+}
+
