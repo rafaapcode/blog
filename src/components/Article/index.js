65a9dd7e24eaf3db80949d2f357d930c5c170119
rafaapcode <rafaap2003@gmail.com>
2024-06-16 21:09:07 -0300

    feature: using props in the components

--- src/components/Article/index.js
+++ src/components/Article/index.js
@@ -5,9 +5,15 @@ export default function Article(){
     <section className="container">
       <h3>Article</h3>
       <div className="mt-5 container-posts">
-        <Posts />
-        <Posts />
-        <Posts />
+        <Posts subtitle="Tecnologia" title="O guia definitivo do Blog">
+          1Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officiis molestiae doloremque ex recusandae dicta! Consectetur officiis quibusdam, ea.
+        </Posts>
+        <Posts subtitle="Fotografia" title="Quais as melhores câmeras para 2021">
+          2Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officiis molestiae doloremque ex recusandae dicta! Consectetur officiis quibusdam, ea.
+        </Posts>
+        <Posts subtitle="Cinema" title="Os 10 filmes com maior bilheteria">
+          3Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officiis molestiae doloremque ex recusandae dicta! Consectetur officiis quibusdam, ea.
+        </Posts>
       </div>
     </section>
   )
--- src/components/Posts/index.js
+++ src/components/Posts/index.js
@@ -1,14 +1,10 @@
-export default  function Posts() {
+export default  function Posts({subtitle, title, children}) {
   return (
    <div className="post mx-2">
       <div className="img-post"></div>
-      <h5>Tecnologia</h5>
-      <h3>O guia definitivo do Blog</h3>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-        Iusto totam itaque aliquam adipisci, voluptate 
-        atque tenetur quia enim voluptatibus saepe aut eos laudantium qui, 
-        eveniet esse optio similique nisi expedita?
-      </p>
+      <h5>{subtitle}</h5>
+      <h3>{title}</h3>
+      <p>{children}</p>
       <div className="flex pt-2">
         <div className="img-profile"></div>
         <div className="desc-profile ml-2">
